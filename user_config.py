d08e07d2f5106f5db4a412ac13bcbcb6614b6229
gongshang <107874313+gongshang0@users.noreply.github.com>
2024-08-08 23:40:43 +0800

    user_config.py

--- user_config.py
+++ user_config.py
@@ -1,3 +1,5 @@
+open_update = True
+open_use_old_result = True
 source_file = "user_demo.txt"
 final_file = "user_result.txt"
 favorite_list = [
@@ -7,12 +9,12 @@ favorite_list = [
     "CCTV-13",
     "浙江卫视",
     "湖南卫视",
-    "翡翠台",
 ]
-open_online_search = False
+open_online_search = True
 favorite_page_num = 5
 default_page_num = 3
 urls_limit = 15
+open_keep_all = False
 open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
@@ -28,3 +30,6 @@ subscribe_urls = [
 ]
 open_multicast = True
 region_list = ["all"]
+open_proxy = True
+open_driver = False
+open_use_old_result = True
